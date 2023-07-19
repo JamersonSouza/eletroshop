39eff7b36a33201b3bbf95a867eebc9c06549aea
jamersonsouza <jamersonsouza@outlook.com>
2023-07-19 20:52:03 -0300

    iniciando o projeto

---
+++ src/main/java/com/jamersondev/eletroshop/EletroshopApplication.java
@@ -0,0 +1,13 @@
+package com.jamersondev.eletroshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletroshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletroshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamersondev/eletroshop/EletroshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamersondev.eletroshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletroshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
