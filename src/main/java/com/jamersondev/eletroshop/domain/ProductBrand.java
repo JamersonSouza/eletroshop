46abbefbf717d2097b74dfdce52ae501c74b32b8
jamersonsouza <jamersonsouza@outlook.com>
2023-07-19 21:38:50 -0300

    Classe para a marca de um produto

--- src/main/java/com/jamersondev/eletroshop/domain/ProductBrand.java
+++ src/main/java/com/jamersondev/eletroshop/domain/ProductBrand.java
@@ -1,2 +1,27 @@
-package com.jamersondev.eletroshop.domain;public class ProductBrand {
+package com.jamersondev.eletroshop.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "product_brand")
+@SequenceGenerator(name = "seq_product_brand", sequenceName = "seq_product_brand", allocationSize = 1, initialValue = 1)
+@Getter
+@Setter
+public class ProductBrand implements Serializable {
+    private static final long serialVersionUID = -6585019090621275453L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name_brand")
+    private String name;
+
 }
