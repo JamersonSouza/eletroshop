956eed5726dd90f1592d2e040ea3b2160c23c1c9
jamersonsouza <jamersonsouza@outlook.com>
2023-07-19 22:01:18 -0300

    Classe para categoria de um produto modelada e alteração no mysql dialect

---
+++ src/main/java/com/jamersondev/eletroshop/domain/ProductCategory.java
@@ -0,0 +1,39 @@
+package com.jamersondev.eletroshop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "product_category")
+@Getter
+@Setter
+public class ProductCategory implements Serializable {
+
+    private static final long serialVersionUID = -6856871570432348208L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name_category")
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductCategory that = (ProductCategory) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
