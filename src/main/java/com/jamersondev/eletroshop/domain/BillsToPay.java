42d28f38209af552ccadff81b40273a0369d964d
jamersonsouza <jamersonsouza@outlook.com>
2023-07-22 13:48:56 -0300

    remoção de imports

--- src/main/java/com/jamersondev/eletroshop/domain/BillsToPay.java
+++ src/main/java/com/jamersondev/eletroshop/domain/BillsToPay.java
@@ -1,12 +1,10 @@
 package com.jamersondev.eletroshop.domain;
 
 import com.jamersondev.eletroshop.enums.BillsToPayStatus;
-import com.jamersondev.eletroshop.enums.BillsToReceiveStatus;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
