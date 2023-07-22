88079813cfc7c16d9b2f0da7d68be64c2277af0e
jamersonsouza <jamersonsouza@outlook.com>
2023-07-22 12:42:58 -0300

    equals e hashcodes gerados e ajuste no relacionamento com PersonPJ

--- src/main/java/com/jamersondev/eletroshop/domain/BillsToReceive.java
+++ src/main/java/com/jamersondev/eletroshop/domain/BillsToReceive.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @Table(name = "bills_to_receive")
@@ -38,12 +39,19 @@ public class BillsToReceive implements Serializable {
     private BigDecimal discountValue;
 
     @ManyToOne
-    @JoinColumn(name = "personPf_id", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "personPf_fk"))
-    private PersonPF personPf;
-
-    @ManyToOne
-    @JoinColumn(name = "personPj_id", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "personPj_fk"))
+    @JoinColumn(name = "personPj_id", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "personPj_billsToReceive_fk"))
     private PersonPF personPj;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BillsToReceive that = (BillsToReceive) o;
+        return Objects.equals(id, that.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
