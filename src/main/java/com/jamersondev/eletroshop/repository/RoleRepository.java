22fe557118e08a5d4dc59ebe233f229d5029d961
jamersonsouza <jamersonsouza@outlook.com>
2023-07-31 22:34:06 -0300

    testes e list no repository de roles

--- src/main/java/com/jamersondev/eletroshop/repository/RoleRepository.java
+++ src/main/java/com/jamersondev/eletroshop/repository/RoleRepository.java
@@ -2,9 +2,15 @@ package com.jamersondev.eletroshop.repository;
 
 import com.jamersondev.eletroshop.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    @Query(value = "SELECT a FROM role_access a WHERE UPPER(TRIM(a.access_role_type)) LIKE %?1%", nativeQuery = true)
+    List<Role> findRoleAccess(String desc);
 
 }
--- src/test/java/com/jamersondev/eletroshop/RoleTests.java
+++ src/test/java/com/jamersondev/eletroshop/RoleTests.java
@@ -4,12 +4,13 @@ import com.jamersondev.eletroshop.domain.Role;
 import com.jamersondev.eletroshop.enums.RoleAccess;
 import com.jamersondev.eletroshop.repository.RoleRepository;
 import com.jamersondev.eletroshop.service.RoleService;
+import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class RoleTests {
+public class RoleTests extends TestCase {
     @Autowired
     private RoleService roleService;
 
@@ -19,6 +20,9 @@ public class RoleTests {
         Role role = new Role();
         role.setRole(RoleAccess.ROLE_ADMIN);
         roleService.rolePersistence(role);
+
+        assertTrue(role.getId() > 1);
+        assertEquals(RoleAccess.ROLE_ADMIN, role.getRole());
     }
 
 }
