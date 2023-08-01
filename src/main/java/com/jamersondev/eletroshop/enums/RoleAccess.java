bbbc1f92ec64a40b734e510efa5d7800bb22f7c9
jamersonsouza <jamersonsouza@outlook.com>
2023-07-31 22:33:27 -0300

    correção para deserialização do enum

--- src/main/java/com/jamersondev/eletroshop/enums/RoleAccess.java
+++ src/main/java/com/jamersondev/eletroshop/enums/RoleAccess.java
@@ -1,9 +1,13 @@
 package com.jamersondev.eletroshop.enums;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum RoleAccess {
     ROLE_USER("User"),
     ROLE_ADMIN("Administrador");
 
+    @JsonValue
     private String roleDescripton;
 
     RoleAccess(String roleDescripton) {
